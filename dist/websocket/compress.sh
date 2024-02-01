a8225afd3eb70123fc0227b3cd7195772e57e636
tam888888 <trinhvan.tam.group.8@gmail.com>
2024-02-01 09:27:49 +0000

    add

--- dist/websocket/compress.sh
+++ dist/websocket/compress.sh
@@ -1,4 +1,13 @@
 d=`date +'%Y%m%d'`
-7zz a data$d".7z" data$d".txt"
-7zz a data3$d".7z" data3$d".txt"
-7zz a data_ps_$d".7z" data_ps_$d".txt"
+cd /home/hungsex/server/xdata/dist/websocket
+NODE_HOME=/home/hungsex/.nvm/versions/node/v16.20.2
+PATH=$NODE_HOME/bin:$PATH
+export PATH
+/home/hungsex/.nvm/versions/node/v16.20.2/bin/pm2 stop 0  >> /home/hungsex/server/dist/dist/cron.log 2>&1
+/usr/bin/7z a data3$d".7z" data3$d".txt"
+/home/hungsex/.nvm/versions/node/v16.20.2/bin/pm2 start 0  >> /home/hungsex/server/dist/dist/cron.log 2>&1
+/usr/bin/git add *.7z
+/bin/sleep 5
+/usr/bin/git commit -m "add"
+/bin/sleep 5
+/usr/bin/git push
