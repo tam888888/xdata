a5c7ddca7bd351144fe2eaba2364939eee6368ee
tam888888 <trinhvan.tam.group.8@gmail.com>
2024-02-06 23:30:03 +0000

    add

--- commit.sh
+++ commit.sh
@@ -1,5 +1,5 @@
 cd /home/hungsex/server/xdata
-/usr/bin/find . -size +50M -printf '%P\n' >> 50M.txt
+/usr/bin/find . -size +50M -printf '%P\n' > 50M.txt
 /bin/cat gitignore 50M.txt  > .gitignore
 /usr/bin/git add .
 /usr/bin/git commit -m "add"
