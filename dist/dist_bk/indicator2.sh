4ac90790d2cf272095d1f3fd9f3e43819f243a1e
tam888888 <trinhvan.tam.group.8@gmail.com>
2025-05-23 22:42:03 +0700

    ok

--- dist/dist_bk/indicator2.sh
+++
@@ -1,5 +0,0 @@
-cd /home/hungsex/server/dist/dist
-NODE_HOME=/home/hungsex/.nvm/versions/node/v16.20.2
-PATH=$NODE_HOME/bin:$PATH
-export PATH
-FORCE=T /home/hungsex/.nvm/versions/node/v16.20.2/bin/node --max-old-space-size=4096 indicatorV2.js  >> /home/hungsex/server/dist/dist/cron.log 2>&1
