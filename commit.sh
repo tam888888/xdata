958ff9bc11290510fea3fe8b90ac69011433696d
tam888888 <trinhvan.tam.group.8@gmail.com>
2025-05-31 11:03:45 +0700

    add

--- commit.sh
+++ commit.sh
@@ -1,3 +1,5 @@
+git config --global user.email "trinhvan.tam.group.8@gmail.com"
+git config --global user.name "tam888888"
 cd /home/hungsex/server/xdata
 /usr/bin/find . -size +50M -printf '%P\n' > 50M.txt
 /bin/cat gitignore 50M.txt  > .gitignore
