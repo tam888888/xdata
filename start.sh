7e4fe42280076960635f79e3cef8ac48ec956713
tam888888 <trinhvan.tam.group.8@gmail.com>
2025-07-31 23:30:08 +0700

    add

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ cd /home/hungtv/server; pm2 start "http-server -p 8888 -g true -b true . --cors"
 pm2 start "cloudflared tunnel run stock-tunnel" --name cloudflared
 pm2 start "cloudflared tunnel --url http://localhost:8888" --name tunnellocalhost8888
 cd /home/hungtv/server/dist; pm2 start websocket3.js --name websocket
-cd /home/hungtv/server/dist/dist; export PORT=8080; pm2 start app.js --name app
+cd /home/hungtv/server/dist/dist; export PORT=8080; pm2 start app.js --name app  --max-memory-restart 5G
